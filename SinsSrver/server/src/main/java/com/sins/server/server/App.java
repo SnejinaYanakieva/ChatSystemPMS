92c3c012abf73b1915ce2ccb4b52a5a263988fbd
SnejinaYanakieva <33015949+SnejinaYanakieva@users.noreply.github.com>
2017-11-07 08:43:41 +0200

    added SinsServer multi module project to trunk

---
+++ SinsSrver/server/src/main/java/com/sins/server/server/App.java
@@ -0,0 +1,13 @@
+package com.sins.server.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
