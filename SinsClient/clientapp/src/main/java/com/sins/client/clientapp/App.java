68f8c63f099da15d8ca881847d5239e270cf0f1f
SnejinaYanakieva <33015949+SnejinaYanakieva@users.noreply.github.com>
2017-11-12 15:29:16 +0200

    added clientapp module to client project

---
+++ SinsClient/clientapp/src/main/java/com/sins/client/clientapp/App.java
@@ -0,0 +1,13 @@
+package com.sins.client.clientapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
