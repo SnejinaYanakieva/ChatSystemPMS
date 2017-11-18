28a1535632ca33be0fe27af317065ff0d31839c2
iDrunkenFist <spe6en@abv.bg>
2017-11-18 16:04:34 +0200

    Test za skapaniq FXML

--- SinsClient/gui/src/main/java/com/sins/client/gui/App.java
+++ SinsClient/gui/src/main/java/com/sins/client/gui/App.java
@@ -1,13 +1,36 @@
 package com.sins.client.gui;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends Application
 {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        primaryStage.initStyle(StageStyle.TRANSPARENT); // removes the window buttons
+        
+        Scene scene = new Scene(root, Color.rgb(0, 0, 0, 0)); // sets transparent background x1 (x2 is in fxml)
+        
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!\n my name Jeff" );
+        launch(args);
     }
 }
