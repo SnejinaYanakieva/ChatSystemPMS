75bbac28560e78fe8bc45b06c02d7330abc288c1
SnejinaYanakieva <33015949+SnejinaYanakieva@users.noreply.github.com>
2017-12-02 21:17:23 +0200

    changes to resolver

--- SinsServer/server/src/main/java/com/sins/server/resolver/Resolver.java
+++ SinsServer/server/src/main/java/com/sins/server/resolver/Resolver.java
@@ -5,9 +5,7 @@
  */
 package com.sins.server.resolver;
 
-import java.math.BigDecimal;
 import java.util.HashMap;
-import java.util.Map;
 import javax.json.Json;
 import javax.json.JsonObject;
 
@@ -24,15 +22,119 @@ public class Resolver {
         String type = json.getString("type");
         switch (type) {
             case "USER":
+                resolveUserTypeRequests(json);
                 break;
             case "FRIEND":
+                resolveFriendTypeRequests(json);
                 break;
             case "GROUP":
+                resolveGroupTypeRequests(json);
                 break;
             case "CHAT":
+                resolveChatTypeRequests(json);
                 break;
             default:
                 String errorContent = "Requested type not recognized!";
+                JsonObject response = buildJson(json, false, Json
+                        .createObjectBuilder()
+                        .add("errorMessage", errorContent)
+                        .build());
+                responseMap.put(json.getString("clientId"), response);
+        }
+        return responseMap;
+    }
+
+    private HashMap<String, JsonObject> resolveUserTypeRequests(JsonObject json) {
+        HashMap<String, JsonObject> responseMap = new HashMap<String, JsonObject>();
+        String subtype = json.getString("subtype");
+        switch (subtype) {
+            case "register":
+                break;
+            case "login":
+                break;
+            case "readPersonalInfo":
+                break;
+            case "updatePersonalInfo":
+                break;
+            case "logout":
+                break;
+            case "deleteUser":
+                break;
+            default:
+                String errorContent = "Requested USER type subtype not recognized!";
+                JsonObject response = buildJson(json, false, Json
+                        .createObjectBuilder()
+                        .add("errorMessage", errorContent)
+                        .build());
+                responseMap.put(json.getString("clientId"), response);
+        }
+        return responseMap;
+    }
+
+    private HashMap<String, JsonObject> resolveFriendTypeRequests(JsonObject json) {
+        HashMap<String, JsonObject> responseMap = new HashMap<String, JsonObject>();
+        String subtype = json.getString("subtype");
+        switch (subtype) {
+            case "createGroup":
+                break;
+            case "addFriendToGroup":
+                break;
+            case "removeFriendFromGroup":
+                break;
+            case "getAllGroupParticipants":
+                break;
+            case "deleteGroup":
+                break;
+            default:
+                String errorContent = "Requested USER type subtype not recognized!";
+                JsonObject response = buildJson(json, false, Json
+                        .createObjectBuilder()
+                        .add("errorMessage", errorContent)
+                        .build());
+                responseMap.put(json.getString("clientId"), response);
+        }
+        return responseMap;
+    }
+
+    private HashMap<String, JsonObject> resolveChatTypeRequests(JsonObject json) {
+        HashMap<String, JsonObject> responseMap = new HashMap<String, JsonObject>();
+        String subtype = json.getString("subtype");
+        switch (subtype) {
+            case "sendMessageToFriend":
+                break;
+            case "sendFileAcceptRequest":
+                break;
+            case "sendFileAcceptResponse":
+                break;
+            case "sendFileToFriend":
+                break;
+            case "sendMessageToGroup":
+                break;
+            default:
+                String errorContent = "Requested CHAT type subtype not recognized!";
+                JsonObject response = buildJson(json, false, Json
+                        .createObjectBuilder()
+                        .add("errorMessage", errorContent)
+                        .build());
+                responseMap.put(json.getString("clientId"), response);
+        }
+        return responseMap;
+    }
+
+    private HashMap<String, JsonObject> resolveGroupTypeRequests(JsonObject json) {
+        HashMap<String, JsonObject> responseMap = new HashMap<String, JsonObject>();
+        String subtype = json.getString("subtype");
+        switch (subtype) {
+            case "getAllFriends":
+                break;
+            case "searchNewFriend":
+                break;
+            case "addNewFriend":
+                break;
+            case "removeFriend":
+                break;
+            default:
+                String errorContent = "Requested GROUP type subtype not recognized!";
                 JsonObject response = buildJson(json, false, Json
                         .createObjectBuilder()
                         .add("errorMessage", errorContent)
