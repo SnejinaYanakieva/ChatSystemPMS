ee4d019cc806a502169812705b81d86d9e8ca3de
SnejinaYanakieva <33015949+SnejinaYanakieva@users.noreply.github.com>
2017-12-02 21:28:39 +0200

    changes to endpoint to use resolver

--- SinsServer/server/src/main/java/com/sins/server/server/ServerEndpoint.java
+++ SinsServer/server/src/main/java/com/sins/server/server/ServerEndpoint.java
@@ -5,17 +5,17 @@
  */
 package com.sins.server.server;
 
+import com.sins.server.resolver.Resolver;
 import java.io.IOException;
+import java.io.StringReader;
 import static java.lang.String.format;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Collectors;
 import javax.json.Json;
 import javax.json.JsonObject;
-import static javax.management.Query.value;
+import javax.json.JsonReader;
 import javax.websocket.EncodeException;
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
@@ -28,32 +28,35 @@ import javax.websocket.Session;
  */
 @javax.websocket.server.ServerEndpoint(value = "/chat")
 public class ServerEndpoint {
-    
+
     static Map<String, Session> peers = Collections.synchronizedMap(new HashMap<String, Session>());
-    
+
     @OnOpen
     public void onOpen(Session session) {
         System.out.println(format("%s joined the chat room.", session.getId()));
     }
-    
+
     @OnMessage
     public void onMessage(String message, Session session) throws IOException, EncodeException {
-        
+        JsonReader jsonReader = Json.createReader(new StringReader(message));
+        JsonObject jsonMessage = jsonReader.readObject();
+        jsonReader.close();
+        Resolver.INSTANCE.resolve(jsonMessage);
     }
-    
+
     @OnClose
     public void onClose(Session session) throws IOException, EncodeException {
         System.out.println(format("%s left the chat room.", session.getId()));
-           peers.entrySet()
+        peers.entrySet()
                 .stream()
                 .filter(entry -> Objects.equals(entry.getValue(), session))
                 .map(Map.Entry::getKey)
                 .forEach(clientid -> peers.remove(clientid));
     }
 
-    public static void sendMessage(Map<String, JsonObject> messageJson) throws IOException{
-        for(Map.Entry<String, JsonObject>message : messageJson.entrySet()){
-             peers.get(message.getKey()).getBasicRemote().sendText(message.getValue().toString());
+    public static void sendMessage(Map<String, JsonObject> messageJson) throws IOException {
+        for (Map.Entry<String, JsonObject> message : messageJson.entrySet()) {
+            peers.get(message.getKey()).getBasicRemote().sendText(message.getValue().toString());
         }
     }
 }
