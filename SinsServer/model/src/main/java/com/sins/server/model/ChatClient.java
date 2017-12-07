8e11bba44bd1f28a745323aedddef539b9ad26e3
zeedor4o <33573228+zeedor4o@users.noreply.github.com>
2017-12-07 16:05:37 +0200

    Add files via upload

---
+++ SinsServer/model/src/main/java/com/sins/server/model/ChatClient.java
@@ -0,0 +1,26 @@
+package com.sins.server.model;
+
+import java.util.List;
+
+public class ChatClient extends Person {
+    
+    private List<Person> friendList;
+    private List<Group> groupLIst;
+
+    public List<Person> getFriendList() {
+        return friendList;
+    }
+
+    public void setFriendList(List<Person> friendList) {
+        this.friendList = friendList;
+    }
+
+    public List<Group> getGroupLIst() {
+        return groupLIst;
+    }
+
+    public void setGroupLIst(List<Group> groupLIst) {
+        this.groupLIst = groupLIst;
+    }
+    
+}
---
+++ SinsServer/model/src/main/java/com/sins/server/model/CurrentClient.java
@@ -0,0 +1,49 @@
+package com.sins.server.model;
+
+import java.util.List;
+
+class CurrentClient extends Person{
+    
+    private String nickname;
+    private String email;
+    private String phone;
+    private String city;
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+    
+    public List<String> validateItem() {
+        List<String> errors = null;
+        return errors;
+    }
+    
+}
