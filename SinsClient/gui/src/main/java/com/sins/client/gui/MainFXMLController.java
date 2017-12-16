39824569bf5af308d45ea03c46c35a502f07ac8c
iDrunkenFist <spe6en@abv.bg>
2017-12-16 20:47:01 +0200

    Fixed login hints, Added more containers to Main

--- SinsClient/gui/src/main/java/com/sins/client/gui/MainFXMLController.java
+++ SinsClient/gui/src/main/java/com/sins/client/gui/MainFXMLController.java
@@ -3,6 +3,8 @@ package com.sins.client.gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,16 +13,26 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 public class MainFXMLController implements Initializable {
 
     @FXML
     private Hyperlink logout_hlink;
+    @FXML
+    private ListView<String> listViewFriends, listViewConversations;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        ObservableList<String> friends = FXCollections.observableArrayList (
+            "Lois", "Ben", "Marco Polo", "The dude in black xD", "MEGAS", "a", "b", "c", "d", "e", "f", "kk", "asd", "sad", "sad");
+        listViewFriends.setItems(friends);
+        
+        ObservableList<String> chats = FXCollections.observableArrayList (
+            "I just took the biggest shit..", "...and then he did...", "Last night was AWESOME!...", "mom HELP...");
+        listViewConversations.setItems(chats);
     }    
     
     @FXML
